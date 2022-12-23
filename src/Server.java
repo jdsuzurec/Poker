6f7d7c478d61c0495f5919c313fd3298370c23c2
直原寿々花 <104682784+jdsuzurec@users.noreply.github.com>
2022-12-23 23:51:57 +0900

    ServerクラスとServerThreadクラスを実装
    Serverクラス：複数クライアントからの接続を待ち受け、通信処理を行うスレッドを作成して起動
    ServerThreadクラス：各クライアントの処理

---
+++ src/Server.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class Server {
+    final static int PORT_NUMBER = 10000;
+    final static int MAX_CONNECTION = 2;
+
+    /*
+     * <summary>
+     * 複数クライアントからの接続を待ち受け、通信処理を行うスレッドを作成して起動
+     * </summary>
+     */
+    public static void main(String[] args) {
+        // TCPポートを指定してサーバソケットを作成
+        ServerSocket serverSocket;
+        int connection_number = 0; // 接続者数
+        try {
+            serverSocket = new ServerSocket(PORT_NUMBER);
+            System.out.println("Serverが起動しました(port=" + serverSocket.getLocalPort() + ")");
+            while (true) {
+                try {
+                    // クライアントからの接続を待ち受け（accept）
+                    Socket socket = serverSocket.accept();
+                    // 定員が上限に達していない限り接続を受け付け、ソケットを作成する
+                    if (connection_number < MAX_CONNECTION) {
+                        connection_number++;
+                        new ServerThread(socket, connection_number).start();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (serverSocket != null) {
+                            serverSocket.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+ * <summary>
+ * 各クライアントの処理
+ * </summary>
+ */
+class ServerThread extends Thread {
+    private int number;
+    private String userName;
+    private Socket socket;
+    private BufferedReader in; // クライアントからの受取用
+    private PrintWriter out; // クライアントへの送信用
+
+    public ServerThread(Socket socket, int number) {
+        this.socket = socket;
+        this.number = number;
+        System.out.println(number + "番目のクライアントからの接続がありました。");
+    }
+
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            // 無限ループでソケットへの入力を監視する
+            while (true) {
+                // 送られてきたメッセージ読み込み
+                String message = in.readLine();
+                if (message != null) {
+                    // exitだったら終了
+                    if (message.equals("exit")) {
+                        break;
+                    }
+                    System.out.println("クライアントからのメッセージ：" + message);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            System.out.println("切断されました " + socket.getRemoteSocketAddress());
+        }
+    }
+}
