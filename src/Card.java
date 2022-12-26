4428d431ff6b7876a7aee143bad6a6abc4c7e372
直原寿々花 <104682784+jdsuzurec@users.noreply.github.com>
2022-12-26 15:44:34 +0900

    Cardクラス生成
    Cardクラス：カードに関する情報を保持

--- src/Card.java
+++ src/Card.java
@@ -1,19 +1,51 @@
-import java.util.Arrays;
-
 /*
  * <summary>
  * カードに関する情報を保持
  * </summary>
  */
 public class Card {
-    private final int MARK_NUMBER = 4, CARD_NUMBER = 13;
-    private final int SPADE = 0, HEART = 1, DIAMOND = 2, CLOVER = 3;
     private final int A = 1, J = 11, Q = 12, K = 13;
-    private boolean[][] deck = new boolean[MARK_NUMBER][CARD_NUMBER];
+    private String mark;
+    private int number;
+    private boolean isHave;
 
-    public Card() {
-        for (boolean[] isHave : deck) {
-            Arrays.fill(isHave, false);
+    public Card(String mark, int number) {
+        this.mark = mark;
+        this.number = number;
+        this.isHave = false;// 最初は手札ではない
+    }
+
+    public String getMark() {
+        return this.mark;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getNumberString() {
+        switch (this.number) {
+            case A:
+                return "A";
+            case J:
+                return "J";
+            case Q:
+                return "Q";
+            case K:
+                return "K";
+            default:
+                return String.valueOf(this.number);
         }
     }
+
+    public boolean getIsHave() {
+        return this.isHave;
+    }
+
+    public void setIsHave(boolean isHave) {
+        this.isHave = isHave;
+    }
 }
+
+// カード型のものを互いに受け渡す
+// GUIに反映するときString型にする
